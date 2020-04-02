4c06eb85590084f347d1c0ca487d009404c8475a
dionixs <dionixs@tutanota.com>
2020-04-02 13:16:28 +0300

    minimax refactoring

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,5 +1,5 @@
 class AI
-  attr_accessor :best_move
+  attr_reader :best_move
 
   def initialize(game, enemy, computer)
     @game = game
@@ -14,47 +14,42 @@ class AI
   # вызывает минимакс функцию для каждой из них (рекурсия)
   # оценивает полученные значения
   # и возвращает наилучшее из них
-  def minimax(board, player, depth = 0, score = {})
+  def minimax(board, player, depth = 0, moves = {})
     # копия доски
     new_board = board.dup
     # индексы свободных клеток
     empty_indices = new_board.empty_cells
 
+    # проверка на терминальное состояние
     return 10 - depth if @game.won?(@computer)
-    return -10 + depth if @game.won?(@enemy)
-    return 0 if empty_indices.empty?
+    return (-10) + depth if @game.won?(@enemy)
+    return 0 if @game.draw?
 
     empty_indices.each do |index|
       new_board.fill_cell(index, player.token)
       next_player = opponent(player)
-      score[index] = minimax(new_board, next_player, depth + 1)
+      moves[index] = minimax(new_board, next_player, depth + 1)
       new_board.reset_cell(index)
     end
 
-    best_score = 0
+    # возвращаем наилучшее значение
+    minimax_score(moves, player)
+  end
 
+  private
+  # метод для оценки полученных значений
+  # возвращает наилучшее значение
+  def minimax_score(moves, player)
     if player.token == @computer.token
-      @best_move, best_score = score.max_by { |_, v| v }
+      @best_move, best_score = moves.max_by { |_, v| v }
     else
-      @best_move, best_score = score.min_by { |_, v| v }
+      @best_move, best_score = moves.min_by { |_, v| v }
     end
 
     best_score
   end
 
-  # метод для проверки на терминальное состояние:
-  # победа (10) / поражение (-10) / ничья (0)
-  def score(game, depth)
-    if game.won?(@computer)
-      10 - depth
-    elsif game.won?(@enemy)
-      -10 + depth
-    else
-      0
-    end
-  end
-
-  # todo
+  # метод для выбора оппонента для текущего игрока
   def opponent(player)
     if player.token == @computer.token
       @enemy
